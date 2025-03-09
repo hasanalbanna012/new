70e73d39015ba1fc2ae6358e38d9ce87956b5c5e
hasanalbanna012 <banna15-5064@diu.edu.bd>
2025-03-09 11:02:30 +0600

    fonts added

--- new_1/lib/screens/home/pages/home.dart
+++ new_1/lib/screens/home/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:new_1/utils/constants/colors.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
@@ -9,8 +10,10 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text(
           "Mega Shop",
-          
+          style: TextStyle(fontFamily: "DMSans",
+          fontWeight: FontWeight.w700, color: FColors.oceanBlue),
         ) ,
       ),
-    )
+    );
   }
+}
